63e1ea32378fa8b73b49f50450ba18889f0f825a
Nam <nam.luuduc@gmail.com>
2025-01-29 23:22:45 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body>
+        <header>
+            <nav>NAV</nav>
+        </header>
+        <main>
+            <section>SECTION 1</section>
+            <section>SECTION 2</section>
+            <section>SECTION 3</section>
+        </main>
+        <footer>FOOTER</footer>
+    </body>
+</html>
